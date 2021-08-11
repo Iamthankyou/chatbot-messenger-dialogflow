5bee79bf95c71fc5b71a6e7a833a0d79ede4d98a
Le Quang Duy <quangduynb00@gmail.com>
2021-08-11 14:23:12 +0700

    Try fix

--- app.js
+++ app.js
@@ -257,9 +257,9 @@ function handleDialogFlowAction(sender, action, messages, contexts, parameters) 
             }
             break;
 
-            handleMessages(messages, sender);
         default:
         //unhandled action, just send back the text
+            handleMessages(messages, sender);
 
     }
 }
