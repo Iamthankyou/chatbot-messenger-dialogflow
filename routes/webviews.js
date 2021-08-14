cde1326bf4e314905fdbdec0eec5ab77a4c8beb6
Le Quang Duy <quangduynb00@gmail.com>
2021-08-14 09:22:52 +0700

    SDK messenger submit form and send return form for user

--- routes/webviews.js
+++ routes/webviews.js
@@ -2,6 +2,7 @@
 
 const config = require('../config');
 const express = require('express');
+const fbservice = require('../services/fb-service');
 
 const router = express.Router();
 
@@ -10,6 +11,14 @@ router.get('/webview', function (req, res) {
     res.render('newsletter-settings');
 });
 
+router.get('/save', function (req, res) {
+    let body = req.query;
+    let topics = body.topics.join(',');
+    let response = `Đã cài đặt nhận tin ${body.newsletter}, chủ đề: ${topics} , nhận mã khuyến mại: ${body.deals} cho psid ${body.psid}`;
+
+    // psid??
+    fbservice.sendTextMessage(body.psid, response);
+});
 
 
 module.exports = router;
