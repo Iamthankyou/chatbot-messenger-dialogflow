ac4420f79294ea61207d74f23fab0889466ce814
Le Quang Duy <quangduynb00@gmail.com>
2021-08-17 17:15:58 +0700

    Try fix

--- app.js
+++ app.js
@@ -247,7 +247,7 @@ const schedule = require('node-schedule');
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0, new schedule.Range(0, 6)];
 rule.hour = 17;
-rule.minute = 14;
+rule.minute = 17;
 rule.tz = 'Asia/Ho_Chi_Minh';
 const job = schedule.scheduleJob(rule, function(){
   console.log('Hello!');
