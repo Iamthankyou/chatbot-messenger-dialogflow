e72051128f46a218a9ac342d84fdfec55b837e96
Le Quang Duy <quangduynb00@gmail.com>
2021-08-17 17:10:04 +0700

    Auto send everty day test.

--- services/scheduleSend.js
+++ services/scheduleSend.js
@@ -3,7 +3,7 @@ const schedule = require('node-schedule');
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0, new schedule.Range(0, 6)];
 rule.hour = 17;
-rule.minute = 10;
+rule.minute = 12;
 rule.tz = 'Asia/Ho_Chi_Minh';
 const job = schedule.scheduleJob(rule, function(){
   console.log('Hello!');
