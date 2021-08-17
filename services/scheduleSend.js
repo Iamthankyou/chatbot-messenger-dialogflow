320e952ecec7815103a18f82a82185ad0c5cc5d4
Le Quang Duy <quangduynb00@gmail.com>
2021-08-17 17:09:19 +0700

    Auto send everyday test

---
+++ services/scheduleSend.js
@@ -0,0 +1,10 @@
+const schedule = require('node-schedule');
+
+const rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+rule.hour = 17;
+rule.minute = 10;
+rule.tz = 'Asia/Ho_Chi_Minh';
+const job = schedule.scheduleJob(rule, function(){
+  console.log('Hello!');
+});s
